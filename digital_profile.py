ed485e3ebd70445ed498fa47916dc9ad49973d02
415-17-phavivaruan <54741@yupparaj.ac.th>
2026-07-02 08:45:44 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("my digital profile")
+
+st.header ("แนะนำตัว")
+st.write("นางสาว ภวิวรัญชน์ จอมชาญพันธ์")
+st.write("ชื่อเล่น กิมกิม")
+st.write("เลขที่ 17 ม.4/15")
+
+st.header("เป้าหมายและสิ่งที่อยากทำในอณาคต")
+st.write("เป้าหมาย: เที่ยวรอบโลก")
+st.write("สิ่งที่อยากเป็นในอณาคต: จิตกร (artist)")
+
